2309804e3a41ff347a9cb452815b5c89fc5d044c
shadow3312 <eludeceti@gmail.com>
2022-12-12 05:36:22 +0100

    fix: adjust profiles alignment

--- frontend/netflix/pages/me.js
+++ frontend/netflix/pages/me.js
@@ -49,17 +49,19 @@ export default function Me() {
             <div className='flex w-screen h-screen items-center justify-center'>
                 <div className='container w-1/2 shadow-xl shadow-indigo-900/20 h-auto py-12 px-8 rounded-lg flex flex-col items-center'>
                   <h3 className='text-white text-4xl text-center font-bold mb-6'>Who's watching ?</h3>
-                  {profiles.map((profile) => (
-                    <div className='flex flex-col items-center cursor-pointer'>
-                      <div className="relative">
-                        <img className="w-24  h-24 ring-2 p-1 ring-gray-100 rounded-full" src="/images/avatar.png" alt={`${user?.name} ${profile.id}`} fill />
-                        <span className="top-0 right-0 absolute  w-5 h-5 bg-netflix-red border-2 border-white dark:border-gray-800 rounded-full"></span>
+                  <div className='flex'>
+                    {profiles.map((profile) => (
+                      <div className='flex flex-col items-center cursor-pointer mr-4'>
+                        <div className="relative">
+                          <img className="w-24  h-24 ring-2 p-1 ring-gray-100 rounded-full" src="/images/avatar.png" alt={`${user?.name} ${profile.id}`} fill />
+                          <span className="top-0 right-0 absolute  w-5 h-5 bg-netflix-red border-2 border-white dark:border-gray-800 rounded-full"></span>
+                        </div>
+                        <div className='mt-4'>
+                          <p className='text-lg text-white'>{user?.name}</p>
+                        </div>
                       </div>
-                      <div className='mt-4'>
-                        <p className='text-lg text-white'>{user?.name}</p>
-                      </div>
-                    </div>
-                  ))}
+                    ))}
+                  </div>
                   <div className='buttons mt-6'>
                     <Button text={'Manage profiles'} additionnalStyle={'border border-white'} iconName="edit" iconStyle={iconStyle} />
                     <Button text={'Logout'} additionnalStyle={'border border-white mx-auto'} iconName={'logout'} iconStyle={iconStyle} onClick={handleLogout} />
