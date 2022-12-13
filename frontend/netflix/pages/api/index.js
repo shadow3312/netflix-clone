4da4e59f6cebded3d9501c25c8fb342bd1fd49a3
shadow3312 <eludeceti@gmail.com>
2022-12-13 22:00:50 +0100

    chore: move api url to .env

--- frontend/netflix/pages/api/index.js
+++ frontend/netflix/pages/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { API_URL } from '../../constants';
+// import { API_URL } from '../../constants';
 
 let BackendAPI = axios.create({
-    baseURL: API_URL+'/api/v1',
+    baseURL: process.env.NEXT_PUBLIC_API_URL+'/api/v1',
     timeout: 10000,
 });
 
