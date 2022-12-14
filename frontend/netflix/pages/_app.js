d021b6038a753a9dcd2c68896dbb47be726a78ac
shadow3312 <eludeceti@gmail.com>
2022-12-14 17:00:21 +0100

    chore: add analytics

--- frontend/netflix/pages/_app.js
+++ frontend/netflix/pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 import {RecoilRoot} from 'recoil'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <RecoilRoot>
         <Component {...pageProps} />
+
+        <Analytics />
     </RecoilRoot>
   ) 
 }
