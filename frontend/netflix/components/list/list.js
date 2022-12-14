780e96415eecd0640e75d96b5775a8b95cbad359
shadow3312 <eludeceti@gmail.com>
2022-12-14 14:03:25 +0100

    fix: correct my list programming logic

--- frontend/netflix/components/list/list.js
+++ frontend/netflix/components/list/list.js
@@ -1,16 +1,20 @@
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { IMDBAPI } from '../../pages/api'
-import { LocalMoviesAtom } from '../../state/atoms'
+import { LocalMoviesAtom, profileAtom } from '../../state/atoms'
+import {useRecoilValue} from 'recoil'
 import {useRecoilState} from 'recoil'
 import Card from '../card/card'
 
 export default function List({title, fetchUrl, isLarge, isRated, local}) {
+    //#region STATE
     const [movies, setMovies] = useState([])
     const [trailer, setTrailer] = useState('')
     const [isError, setError] = useState(false)
     const [errorMessage, setErrorMessage] = useState('')
     const [localMovies, setLocalMovies] = useRecoilState(LocalMoviesAtom)
+    const currentProfile = useRecoilValue(profileAtom)
+    //#endregion
 
     const capitalize = (str) => {
         var word = str.toLowerCase().split(' ');
@@ -41,15 +45,27 @@ export default function List({title, fetchUrl, isLarge, isRated, local}) {
         if (local) {
             setMovies(localMovies)
         }
-    }, [localMovies])
+    }, [localMovies, movies, currentProfile])
     return (
         movies && <div className='-mt-6 mb-8'>
             <h4 className='text-2xl mb-2 text-white font-bold'>{capitalize(title)}</h4>
             <div className='mt-2 overflow-x-scroll overflow-y-hidden list'>
                 <div className='inline-flex  '>
-                {movies?.map((movie, i) => (
-                    <Card key={movie.id} movie={movie} isLarge={isLarge} index={i} isRated={isRated} />
-                ))}
+                {movies?.map((movie, i) => {
+                    if (local) {
+                        if (movie.profile_id === currentProfile.id)
+                        {
+                            return (
+                                <Card key={movie.id} movie={movie} isLarge={isLarge} index={i} isRated={isRated} />
+                            )
+                        }
+                    } else {
+                        return (
+                            <Card key={movie.id} movie={movie} isLarge={isLarge} index={i} isRated={isRated} />
+                        )
+                    }
+                }
+                )}
                 </div>
             </div>
             </div>
--- frontend/netflix/components/main/main.js
+++ frontend/netflix/components/main/main.js
@@ -21,16 +21,16 @@ export default function Main() {
             <Navbar />
             <Banner fetchUrl={requests.getTrending} />
             <main className="main pl-12 relative" style={{zIndex: 'auto'}}>
-                <List title="trending now" fetchUrl={requests.getTrending} isLarge={true} />
-                <List title="horror movies" fetchUrl={requests.getHorrorMovies} isLarge={true} />
+                <List title="trending now" fetchUrl={requests.getTrending} isLarge={true} local={false} />
+                <List title="horror movies" fetchUrl={requests.getHorrorMovies} isLarge={true} local={false} />
                 <List title="my list" local={true} />
-                <List title="only on netflix" fetchUrl={requests.getNetflixOriginals} isLarge={false} />
-                <List title="action" fetchUrl={requests.getActionMovies} isLarge={true} />
-                <List title="top 10 in France today" fetchUrl={requests.getTopRated} isLarge={false} isRated={true} />
-                <List title="comedies" fetchUrl={requests.getComedyMovies} isLarge={true} />
-                <List title="documentaries" fetchUrl={requests.getDocumentaries} isLarge={true} />
-                <List title="for kids" fetchUrl={requests.getKidMovies} isLarge={true} />
-                <List title="Science Fiction" fetchUrl={requests.getSciFiMovie} isLarge={true} />
+                <List title="only on netflix" fetchUrl={requests.getNetflixOriginals} isLarge={false} local={false} />
+                <List title="action" fetchUrl={requests.getActionMovies} isLarge={true} local={false} />
+                <List title="top 10 in France today" fetchUrl={requests.getTopRated} isLarge={false} isRated={true} local={false} />
+                <List title="comedies" fetchUrl={requests.getComedyMovies} isLarge={true} local={false} />
+                <List title="documentaries" fetchUrl={requests.getDocumentaries} isLarge={true} local={false} />
+                <List title="for kids" fetchUrl={requests.getKidMovies} isLarge={true} local={false} />
+                <List title="Science Fiction" fetchUrl={requests.getSciFiMovie} isLarge={true} local={false} />
             </main>
             <MovieDetail />
             <Footer />
