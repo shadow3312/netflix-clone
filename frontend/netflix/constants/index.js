bbc318d792bf2ba84e636e3716f93b8ef08703c7
shadow3312 <eludeceti@gmail.com>
2023-07-04 01:00:29 +0200

    config: add api url to .env

--- frontend/netflix/constants/index.js
+++ frontend/netflix/constants/index.js
@@ -1,96 +1,96 @@
-const API_URL = 'http://localhost:8000';
-const IMDB_API_AKEY = 'b7eff31063043bf7b8120bc59dd95420'
-const APP_NAME = 'Netflix Clone'
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const IMDB_API_AKEY = "b7eff31063043bf7b8120bc59dd95420";
+const APP_NAME = "Netflix Clone";
 
-const BASE_IMG_URL = "https://image.tmdb.org/t/p/original"
+const BASE_IMG_URL = "https://image.tmdb.org/t/p/original";
 const GENRES = [
-    {
-        id: 28,
-        name: 'Action'
-    },
-    {
-        id: 12,
-        name: 'Adventure'
-    },
-    {
-        id: 16,
-        name: 'Animation'
-    },
-    {
-        id: 35,
-        name: 'Comedy'
-    },
-    {
-        id: 80,
-        name: 'Crime'
-    },
-    {
-        id: 99,
-        name: 'Documentary'
-    },
-    {
-        id: 18,
-        name: 'Drama'
-    },
-    {
-        id: 10751,
-        name: 'Family'
-    },
-    {
-        id: 14,
-        name: 'Fantasy'
-    },
-    {
-        id: 36,
-        name: 'History'
-    },
-    {
-        id: 27,
-        name: 'Horror'
-    },
-    {
-        id: 10402,
-        name: 'Music'
-    },
-    {
-        id: 9648,
-        name: 'Mystery'
-    },
-    {
-        id: 10749,
-        name: 'Romance'
-    },
-    {
-        id: 878,
-        name: 'SF'
-    },
-    {
-        id: 10770,
-        name: 'TV Movie'
-    },
-    {
-        id: 53,
-        name: 'Thriller'
-    },
-    {
-        id: 10752,
-        name: 'War'
-    },
-    {
-        id: 10764,
-        name: 'Reality'
-    },
-    {
-        id: 10766,
-        name: 'Soap'
-    },
-    {
-        id: 10767,
-        name: 'Talk'
-    },
-    {
-        id: 37,
-        name: 'Western'
-    }
-]
-export {API_URL, IMDB_API_AKEY, GENRES, BASE_IMG_URL, APP_NAME}
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 12,
+    name: "Adventure",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+  {
+    id: 80,
+    name: "Crime",
+  },
+  {
+    id: 99,
+    name: "Documentary",
+  },
+  {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 10751,
+    name: "Family",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 36,
+    name: "History",
+  },
+  {
+    id: 27,
+    name: "Horror",
+  },
+  {
+    id: 10402,
+    name: "Music",
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+  },
+  {
+    id: 10749,
+    name: "Romance",
+  },
+  {
+    id: 878,
+    name: "SF",
+  },
+  {
+    id: 10770,
+    name: "TV Movie",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 10752,
+    name: "War",
+  },
+  {
+    id: 10764,
+    name: "Reality",
+  },
+  {
+    id: 10766,
+    name: "Soap",
+  },
+  {
+    id: 10767,
+    name: "Talk",
+  },
+  {
+    id: 37,
+    name: "Western",
+  },
+];
+export { API_URL, IMDB_API_AKEY, GENRES, BASE_IMG_URL, APP_NAME };
